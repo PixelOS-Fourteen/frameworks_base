89f3122da8552da085b85de3cbfc4c5b75482fc8
Christian Oder <myself5@carbonrom.org>
2019-11-26 23:35:01 +0100

    base: SystemUI: Introduce DataSwitchTile
    * Based on OnePlus' OxygenOS tile, reworked to work with AOSP toggling without requirements on proprietary telephony-ext features
    
    idoybh (YAAP): Adapt to A12, A14
    
    Change-Id: Ie2e280c07f24f9da6b4ee218b72501a2713ce429

--- packages/SystemUI/src/com/android/systemui/statusbar/connectivity/ConnectivityModule.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/connectivity/ConnectivityModule.kt
@@ -26,6 +26,7 @@ import com.android.systemui.qs.tiles.AirplaneModeTile
 import com.android.systemui.qs.tiles.BluetoothTile
 import com.android.systemui.qs.tiles.CastTile
 import com.android.systemui.qs.tiles.DataSaverTile
+import com.android.systemui.qs.tiles.DataSwitchTile
 import com.android.systemui.qs.tiles.HotspotTile
 import com.android.systemui.qs.tiles.InternetTile
 import com.android.systemui.qs.tiles.InternetTileNewImpl
@@ -169,4 +170,10 @@ interface ConnectivityModule {
                 mapper,
             )
     }
+
+    /** Inject DataSwitchTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(DataSwitchTile.TILE_SPEC)
+    fun bindDataSwitchTile(dataSwitchTile: DataSwitchTile): QSTileImpl<*>
 }
