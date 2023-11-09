b40f4e76c96295231921ceaab4341b783e7cef26
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-11-09 06:03:41 +0530

    PropImitationHooks: Use ASUS_X00HD props to avoid hardware attestation
    Fixes play device integrity after the recent server-side update.
    
    Thanks to https://github.com/chiteroman/PlayIntegrityFix/commit/0c03f4e93f9d07aa87df54be54ffd03a79fd8374
    
    Change-Id: Icd8bb358c7a766806f4b03ed629e41c2ac41ec5d

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -101,7 +101,7 @@ public class PropImitationHooks {
         sIsPhotos = packageName.equals(PACKAGE_GPHOTOS);
 
         if (sIsGms) {
-            dlog("Setting Pixel XL fingerprint for: " + packageName);
+            dlog("Setting ASUS_X00HD fingerprint for: " + packageName);
             spoofBuildGms();
         } else if (!sCertifiedFp.isEmpty() && sIsFinsky) {
             dlog("Setting certified fingerprint for: " + packageName);
@@ -147,10 +147,10 @@ public class PropImitationHooks {
 
     private static void spoofBuildGms() {
         // Alter model name and fingerprint to avoid hardware attestation enforcement
-        setPropValue("FINGERPRINT", "google/marlin/marlin:7.1.2/NJH47F/4146041:user/release-keys");
-        setPropValue("PRODUCT", "marlin");
-        setPropValue("DEVICE", "marlin");
-        setPropValue("MODEL", "Pixel XL");
+        setPropValue("FINGERPRINT", "asus/WW_Phone/ASUS_X00HD_4:7.1.1/NMF26F/14.2016.1801.372-20180119:user/release-keys");
+        setPropValue("PRODUCT", "WW_Phone");
+        setPropValue("DEVICE", "ASUS_X00HD_4");
+        setPropValue("MODEL", "ASUS_X00HD");
         setVersionField("DEVICE_INITIAL_SDK_INT", Build.VERSION_CODES.N_MR1);
     }
 
