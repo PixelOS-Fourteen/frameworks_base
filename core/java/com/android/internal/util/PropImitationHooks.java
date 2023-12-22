Project-Awaken/android_frameworks_base

PIHooks: Spoof more Google apps
Change-Id: I9faf3b3ffeba74130d9af77f191eae46b44f4b40

core/java/com/android/internal/util/PropImitationHooks.java
@@ -44,18 +44,26 @@ public class PropImitationHooks {
     private static final String TAG = "PropImitationHooks";
     private static final boolean DEBUG = SystemProperties.getBoolean("debug.pihooks.log", false);
 
+    private static final String PACKAGE_AIWALLPAPERS = "com.google.android.apps.aiwallpapers";
     private static final String PACKAGE_ARCORE = "com.google.ar.core";
     private static final String PACKAGE_ASI = "com.google.android.as";
+    private static final String PACKAGE_ASSISTANT = "com.google.android.apps.googleassistant";
+    private static final String PACKAGE_EMOJIWALLPAPER = "com.google.android.apps.emojiwallpaper";
     private static final String PACKAGE_FINSKY = "com.android.vending";
     private static final String PACKAGE_GBOARD = "com.google.android.inputmethod.latin";
     private static final String PACKAGE_GMS = "com.google.android.gms";
     private static final String PACKAGE_GPHOTOS = "com.google.android.apps.photos";
+    private static final String PACKAGE_LIVEWALLPAPER = "com.google.pixel.livewallpaper";
     private static final String PACKAGE_NETFLIX = "com.netflix.mediaclient";
+    private static final String PACKAGE_PIXELTHEMES = "com.google.android.apps.customization.pixel";
+    private static final String PACKAGE_PIXELWALLPAPER = "com.google.android.apps.wallpaper.pixel";
     private static final String PACKAGE_SETUPWIZARD = "com.google.android.setupwizard";
     private static final String PACKAGE_SMS_ORGANIZER = "com.microsoft.android.smsorganizer";
     private static final String PACKAGE_SUBSCRIPTION_RED = "com.google.android.apps.subscriptions.red";
     private static final String PACKAGE_TURBO = "com.google.android.apps.turbo";
     private static final String PACKAGE_VELVET = "com.google.android.googlequicksearchbox";
+    private static final String PACKAGE_WALLPAPER = "com.google.android.apps.wallpaper";
+    private static final String PACKAGE_WALLPAPEREFFECTS = "com.google.android.wallpaper.effects";
 
     private static final String PROCESS_GMS_PERSISTENT = PACKAGE_GMS + ".persistent";
     private static final String PROCESS_GMS_UNSTABLE = PACKAGE_GMS + ".unstable";
@@ -149,9 +157,12 @@ public class PropImitationHooks {
         } else if (packageName.equals(PACKAGE_ASI)) {
             dlog("Spoofing Pixel 5 for: " + packageName);
             sPixelFiveProps.forEach(PropImitationHooks::setPropValue);
-        } else if (sSpoofGapps && (packageName.equals(PACKAGE_GBOARD) || packageName.equals(PACKAGE_SETUPWIZARD)
-                || packageName.equals(PACKAGE_SMS_ORGANIZER) || packageName.equals(PACKAGE_SUBSCRIPTION_RED) || packageName.equals(PACKAGE_TURBO)
-                || packageName.equals(PACKAGE_VELVET) || (packageName.equals(PACKAGE_GMS) && processName.equals(PROCESS_GMS_PERSISTENT)))) {
+        } else if (sSpoofGapps && (packageName.equals(PACKAGE_AIWALLPAPERS) || packageName.equals(PACKAGE_ASSISTANT) || packageName.equals(PACKAGE_EMOJIWALLPAPER)
+                || packageName.equals(PACKAGE_GBOARD) || packageName.equals(PACKAGE_LIVEWALLPAPER) || packageName.equals(PACKAGE_PIXELTHEMES)
+                || packageName.equals(PACKAGE_PIXELWALLPAPER) || packageName.equals(PACKAGE_SETUPWIZARD) || packageName.equals(PACKAGE_SMS_ORGANIZER)
+                || packageName.equals(PACKAGE_SUBSCRIPTION_RED) || packageName.equals(PACKAGE_TURBO) || packageName.equals(PACKAGE_VELVET)
+                || packageName.equals(PACKAGE_WALLPAPER) || packageName.equals(PACKAGE_WALLPAPEREFFECTS)
+                || (packageName.equals(PACKAGE_GMS) && processName.equals(PROCESS_GMS_PERSISTENT)))) {
             dlog("Spoofing Pixel 8 Pro for: " + packageName + " process: " + processName);
             sPixelEightProps.forEach(PropImitationHooks::setPropValue);
         } else if (sIsPhotos) {

PixelOS-Fourteen/frameworks_base

core/java/com/android/internal/util/PropImitationHooks.java
@@ -43,10 +43,25 @@ public class PropImitationHooks {
     private static final String TAG = "PropImitationHooks";
     private static final boolean DEBUG = SystemProperties.getBoolean("debug.pihooks.log", false);
 
+    private static final String PACKAGE_AIWALLPAPERS = "com.google.android.apps.aiwallpapers";
     private static final String PACKAGE_ARCORE = "com.google.ar.core";
+    private static final String PACKAGE_ASSISTANT = "com.google.android.apps.googleassistant";
     private static final String PACKAGE_ASI = "com.google.android.as";
+    private static final String PACKAGE_EMOJIWALLPAPER = "com.google.android.apps.emojiwallpaper";
+
     private static final String PACKAGE_FINSKY = "com.android.vending";
+    private static final String PACKAGE_GBOARD = "com.google.android.inputmethod.latin";
     private static final String PACKAGE_GMS = "com.google.android.gms";
+    private static final String PACKAGE_LIVEWALLPAPER = "com.google.pixel.livewallpaper";
+
+    private static final String PACKAGE_PIXELTHEMES = "com.google.android.apps.customization.pixel";
+    private static final String PACKAGE_PIXELWALLPAPER = "com.google.android.apps.wallpaper.pixel";
+    private static final String PACKAGE_TURBO = "com.google.android.apps.turbo";
+
+    private static final String PACKAGE_SUBSCRIPTION_RED = "com.google.android.apps.subscriptions.red";
+    private static final String PACKAGE_WALLPAPER = "com.google.android.apps.wallpaper";
+    private static final String PACKAGE_WALLPAPEREFFECTS = "com.google.android.wallpaper.effects";
+
     private static final String PACKAGE_GPHOTOS = "com.google.android.apps.photos";
     private static final String PACKAGE_NETFLIX = "com.netflix.mediaclient";
     private static final String PACKAGE_VELVET = "com.google.android.googlequicksearchbox";
@@ -133,8 +148,10 @@ public class PropImitationHooks {
         } else if (!sStockFp.isEmpty() && packageName.equals(PACKAGE_ARCORE)) {
             dlog("Setting stock fingerprint for: " + packageName);
             setPropValue("FINGERPRINT", sStockFp);
-        } else if (sSpoofGapps && (packageName.equals(PACKAGE_VELVET)
-                || packageName.equals(PACKAGE_ASI)
+        } else if (sSpoofGapps && (packageName.equals(PACKAGE_VELVET) || packageName.equals(PACKAGE_SUBSCRIPTION_RED) || packageName.equals(PACKAGE_TURBO)
+                || packageName.equals(PACKAGE_ASI) || packageName.equals(PACKAGE_GBOARD)
+                || packageName.equals(PACKAGE_AIWALLPAPERS) || packageName.equals(PACKAGE_ASSISTANT) || packageName.equals(PACKAGE_EMOJIWALLPAPER) || packageName.equals(PACKAGE_LIVEWALLPAPER)
+                || packageName.equals(PACKAGE_PIXELTHEMES) || packageName.equals(PACKAGE_PIXELWALLPAPER) || packageName.equals(PACKAGE_WALLPAPER)  || packageName.equals(PACKAGE_WALLPAPEREFFECTS)
                 || (packageName.equals(PACKAGE_GMS)
                     && processName.equals(PROCESS_GMS_PERSISTENT)))) {
             dlog("Spoofing Pixel 8 Pro for: " + packageName + " process: " + processName);